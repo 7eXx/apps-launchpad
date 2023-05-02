ddd1fa05a423ac721d25a6148f3a759c08414f2c
Marco Tessari <marco.tessari@fv.lan>
2023-05-02 11:05:51 +0200

    feat: update build script update readme

--- README.md
+++ README.md
@@ -3,4 +3,22 @@ This application aims to create a web component that has the same behavior of go
 See the main google page with shortcut in top right
 
 ## Usage
-The tag for the custom 
+The tag for the custom component:
+```angular2html
+<apps-launchpad></apps-launchpad>
+```
+you can project your custom button using
+```angular2html
+<apps-launchpad>
+  <button slot="toggle-button"
+      type="button" class="btn btn-primary">
+    <span class="fa fa-th"></span>
+  </button>
+</apps-launchpad>
+```
+
+so you can use whatever style you want for the toggle button.
+
+## Screenshot
+![record-1](./screenshot/record-1.gif)
+
--- build-component.js
+++ build-component.js
@@ -1,15 +1,31 @@
-const fs = require('fs-extra');
+const fs = require('fs');
+const fsExtra = require('fs-extra');
 const concat = require('concat');
 
 build = async () => {
-  const files = [
-    './dist/apps-launchpad/runtime.js',
-    './dist/apps-launchpad/polyfills.js',
-    './dist/apps-launchpad/scripts.js',
-    './dist/apps-launchpad/main.js'
-  ];
+  const runtimePath = './dist/apps-launchpad/runtime.js';
+  const polyfillsPath = './dist/apps-launchpad/polyfills.js';
+  const scriptPath = './dist/apps-launchpad/scripts.js';
+  const mainPath = './dist/apps-launchpad/main.js';
 
-  await fs.ensureDir('public');
+  const files = [];
+  if (fs.existsSync(runtimePath)) {
+    files.push(runtimePath);
+  }
+
+  if (fs.existsSync(polyfillsPath)) {
+    files.push(polyfillsPath);
+  }
+
+  if (fs.existsSync(scriptPath)) {
+    files.push(scriptPath);
+  }
+
+  if (fs.existsSync(mainPath)) {
+    files.push(mainPath);
+  }
+
+  await fsExtra.ensureDir('public');
   await concat(files, 'public/apps-launchpad.js');
 }
 
